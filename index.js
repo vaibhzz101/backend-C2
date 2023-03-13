c5dde5929e0553477dd40a5286ffe29bfc13bfac
Vaibhav Shevne <112811407+vaibhzz101@users.noreply.github.com>
2023-03-13 17:12:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {connection} = require("./config/db")
+const {connection} = require('./config/db')
 const {authentication} = require('./middleware/authenticater')
 const {userRouter} = require('./routes/user.routes')
 const {productRouter} = require("./routes/products.routes")
@@ -7,19 +7,22 @@ require('dotenv').config();
 
 const app = express()
 app.use(express.json())
+app.get('/',(req,res)=>{
+    res.send('homeroute of backend')
+})
+
 app.use('/users',userRouter)
 app.use(authentication)
 
 app.use('/products', productRouter)
 
 
-app.get('/',(req,res)=>{
-    res.send('homeroute of backend')
-})
+
+
 
 app.listen(process.env.port,()=>{
     try{
-        dbconnection;
+        connection;
         console.log("server running at port"+ process.env.port)
     }
     catch(err){
